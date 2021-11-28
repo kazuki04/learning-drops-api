e5f44c103c2bb60a20824a772b1453d68a389465
kazuki04 <kazukik0402@gmail.com>
2021-11-28 19:57:26 +0900

    define GetSections method

--- app/models/section.go
+++ app/models/section.go
@@ -1,13 +1,15 @@
 package models
 
 import (
-	"gorm.io/gorm"
+	"time"
 )
 
 type Section struct {
-	gorm.Model
-	Title  string `json:"title"`
-	UserId string `json:"user_id"`
+	ID        uint      `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Title     string    `json:"title"`
+	UserId    string    `json:"user_id"`
 }
 
 func NewSection(title string, user_id string) *Section {
@@ -21,4 +23,10 @@ func (s *Section) Create() *Section {
 	section := &Section{Title: s.Title, UserId: s.UserId}
 	DbConnection.Create(&section)
 	return section
+}
+
+func GetSections(user_id string) []Section {
+	var sections []Section
+	DbConnection.Where("user_id = ?", user_id).Find(&sections)
+	return sections
 }
